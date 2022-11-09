85341ae8cbce4da46f92b7365eedbaf939e44234
SevenAkridge <116559747+SevenAkridge@users.noreply.github.com>
2022-11-09 12:28:44 -0600

    Practicing using comments.

---
+++ PythonComments.py
@@ -0,0 +1,12 @@
+#A very cool comment
+print("Waffles :)") #Comment jumpscare
+#print("This wont print bc # lol get pranked")
+#This comment
+#is lowkey
+#inefficient
+"""
+The tripple
+quotes are
+better for 
+multi-line comments ig
+"""
