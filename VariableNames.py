a31e7386d5483824a37624513c5f2b37111ff3c5
SevenAkridge <116559747+SevenAkridge@users.noreply.github.com>
2022-11-09 12:56:21 -0600

    Practicing naming variables.

---
+++ VariableNames.py
@@ -0,0 +1,11 @@
+myvar = "fish"
+my_var = "fish"
+_my_var = "fish"
+myVar = "fish"
+MYVAR = "fish"
+myvar2 = "fish"
+#Variables only should use a-z, 0-9, and/or "_". 
+myVariableName = "fish"
+MyVariableName = "fish"
+my_variabe_name = "fish"
+#Don't use spaces when naming variables
