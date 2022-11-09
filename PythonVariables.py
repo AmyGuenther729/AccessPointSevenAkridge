c7f97831bd07f45e1edb8e05f6cf57b1e3f1063d
SevenAkridge <116559747+SevenAkridge@users.noreply.github.com>
2022-11-09 12:50:16 -0600

    Practicing creating and using variables.

---
+++ PythonVariables.py
@@ -0,0 +1,31 @@
+x = 7   
+y="fish" 
+#Note:spacing doesn't matter
+print(x)
+print(y)
+
+x = 5   # x as an integer
+x = "five"   # x is as a string
+print(x)
+#An older one is overridden by the newest one 
+
+x = str(7)   # str = '3'
+y = int(7)   # int = 3
+z = float(7) # float = 3.0
+#This is called Casting.
+#integer vs. float: float uses decimals, int is whole numbers
+
+x = 7
+y = "fish"
+print(type(x))
+print(type(y))
+#This tells you what type of data this is. 
+
+x = "fish"
+x = 'fish'
+#string variables use both single & double quotes. They don't make a difference.
+
+a = 2
+A = "Two"
+print(a)
+#variables are case sensitive
